9624586bb18895cc0626ccffb05fae4f314a5389
SABELO MAWELA <MAWELAS63@GMAIL.COM>
2024-06-11 03:51:29 +0200

    "Updated showReviewTotal function to accept two parameters and modified its call to pass reviewer name"

--- index.ts
+++ index.ts
@@ -24,8 +24,8 @@ const reviews = [
         date: '27-03-2021'
     },
 ]
-function showReviewTotal (value : number) {
-    reviewTotalDisplay.innerHTML = value.toString()
+function showReviewTotal (value : number, reviewer: string) {
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer
 }
 
-showReviewTotal(reviews.length)
+showReviewTotal(reviews.length, reviews[0].name)
